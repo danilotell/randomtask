90f96e7a613f07da4238e923ea32af4b3a721382
Danilo Peña <chrdanilo@gmail.com>
2021-03-19 18:40:46 -0500

    v1.0.0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 let tareas = [];
+let turno = true;
+
 
 class Task{
     constructor(name){
@@ -20,11 +22,32 @@ class UI{
 
         tareas.push(TN.name);
         console.log(tareas);
+        this.resetForm();
+
+    }
+
+    randomPick(){
+        let x = Math.floor(Math.random() * tareas.length);
+        return x
+    }
+
+    randomTask(rp, tareas, setElement){
+        const allTaskList = document.getElementById(setElement);
+        const element = document.createElement("A");
+        element.setAttribute('class','list-group-item list-group-item-action list-group-item-dark')
+        const textnode = document.createTextNode(tareas[rp]);
+        element.appendChild(textnode);
+        allTaskList.appendChild(element);
+        tareas.splice(rp,1)
 
     }
 
     removeTask(){
 
+    }
+
+    resetForm(){
+        document.getElementById('tasks-form').reset();
     }
 }
 
@@ -34,10 +57,23 @@ document.getElementById('tasks-form')
 .addEventListener('submit', function(e){
     const getTaskName = document.getElementById('inlineFormInputTask').value;
     const newTask = new Task(getTaskName);
-    
     const ui = new UI();
     ui.addTask(newTask);
+    e.preventDefault();
+});
 
+
+document.getElementById('tasks-pick')
+.addEventListener('submit', function(e){
+    const ui = new UI();
+    const rp =  ui.randomPick();
+    if (tareas.length > 0 && turno == true){
+        ui.randomTask(rp, tareas, "task-list-p1");
+        turno = false;
+    } else if(tareas.length > 0 && turno == false){
+        ui.randomTask(rp, tareas, "task-list-p2");
+        turno = true;
+    }
     e.preventDefault();
 });
 
