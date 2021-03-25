525c7a327b6ff7bd8b2379673030863a8fa7dc99
danigami <chrdanilo@gmail.com>
2021-03-24 22:38:19 -0500

    alpha release

--- js/App.js
+++ js/App.js
@@ -1,5 +1,6 @@
 let tareas = [];
 let turno = true;
+let id = 0;
 
 
 class Task{
@@ -18,10 +19,12 @@ class UI{
             const allTaskList = document.getElementById('task-list');
             const element = document.createElement("A");
             element.setAttribute('class','list-group-item list-group-item-action list-group-item-dark')
+            element.setAttribute('id', id)
             const textnode = document.createTextNode(TN.name);
             element.appendChild(textnode);
             allTaskList.appendChild(element);
             tareas.push(TN.name);
+            id++;
             console.log(tareas);
             this.resetForm();
         }
@@ -38,16 +41,21 @@ class UI{
     randomTask(rp, tareas, setElement){
         const allTaskList = document.getElementById(setElement);
         const element = document.createElement("A");
-        element.setAttribute('class','list-group-item list-group-item-action list-group-item-dark')
+        element.setAttribute('class','list-group-item list-group-item-action list-group-item-dark');
         const textnode = document.createTextNode(tareas[rp]);
         element.appendChild(textnode);
         allTaskList.appendChild(element);
-        tareas.splice(rp,1)
+        try {
+            this.removeTask(rp);
+          } catch (error) {
+            console.error(error);
+          }
+        tareas.splice(rp,1);
 
     }
 
-    removeTask(){
-
+    removeTask(id){
+        document.getElementById(id).remove();
     }
 
     resetForm(){
@@ -63,6 +71,9 @@ document.getElementById('tasks-form')
     const newTask = new Task(getTaskName);
     const ui = new UI();
     ui.addTask(newTask);
+    if (tareas.length == 1){
+        ui.removeTask("no0");
+    }
     e.preventDefault();
 });
 
@@ -72,10 +83,22 @@ document.getElementById('tasks-pick')
     const ui = new UI();
     const rp =  ui.randomPick();
     if (tareas.length > 0 && turno == true){
+        try {
+            ui.removeTask("no1");
+          } catch (error) {
+            console.error(error);
+          }
         ui.randomTask(rp, tareas, "task-list-p1");
+        //ui.removeTask(rp)
         turno = false;
     } else if(tareas.length > 0 && turno == false){
+        try {
+            ui.removeTask("no2");
+          } catch (error) {
+            console.error(error);
+          }
         ui.randomTask(rp, tareas, "task-list-p2");
+        //ui.removeTask(rp)
         turno = true;
     }
     e.preventDefault();
